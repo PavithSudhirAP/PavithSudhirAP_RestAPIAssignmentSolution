14a903cc69f0396f1393d7323b32b485a47b8cf1
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2022-08-03 11:54:51 +0530

    Delete GlRestApiProject directory

--- GlRestApiProject/src/main/java/com/greatlearning/employeemanagement/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.employeemanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.employeemanagement.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer>{
-
-}
